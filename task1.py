d62010d1655a176dee29ee4cdd18f907b84053db
yogesh-a11y <yogeshbasnet88@gmail.com>
2023-02-26 21:12:56 +0545

    Count Numbers

--- task1.py
+++ task1.py
@@ -3,3 +3,7 @@ Area_of_circle = (22/7)*radius**2
 print(Area_of_circle)
 
 #Change
+l = [1,2,4,2,1,4,5,6,2,1,6,3]
+number = int(input("Enter a desire number:"))
+count = l.count(number)
+print(count)
